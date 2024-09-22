9c47683fd329747ca6e7d3bdb1688299afa09faa
ChauHuyDien <82036270+idiotman-2212@users.noreply.github.com>
2024-09-22 16:43:37 +0700

    update multi language

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ import useLogin from "./utils/useLogin";
 import ChangePassword from "./scenes/ChangePassword";
 import ForgotPwd from "./scenes/ForgotPwd";
 import "./utils/i18n"; // Import cấu hình i18n
+import { useEffect } from 'react';
 
 //kiểm tra quyền người dùng 
 const hasReadAccessRoleToScreen = (permissions, pathname) => {
@@ -82,6 +83,25 @@ const App = () => {
       },
     },
   });
+
+  useEffect(() => {
+    (function(d, m) {
+      var kommunicateSettings = {
+        "appId": "291034e15da45c3b1f0f79c535bfe8dda", // Thay YOUR_APP_ID bằng App ID của bạn
+        "popupWidget": true,
+        "automaticChatOpenOnNavigation": true
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }, []);
+  
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,14 +1,22 @@
-import { React, useContext } from "react";
-import { Box, IconButton, InputBase, useTheme, Tooltip  } from "@mui/material";
+import { React, useContext, useState } from "react";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  useTheme,
+  Tooltip,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import { ColorModeContext, tokens } from "../../theme";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
-import { FaFlagUsa } from 'react-icons/fa';
-import { FaFlag } from 'react-icons/fa';
-import { initReactI18next, useTranslation } from 'react-i18next';
+import { FaFlagUsa } from "react-icons/fa";
+import { FaFlag } from "react-icons/fa";
+import { initReactI18next, useTranslation } from "react-i18next";
 import vietnamFlag from "../../assets/vietnam.png";
 import usaFlag from "../../assets/uk.png";
 
@@ -16,12 +24,20 @@ const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
-  const {t, i18n} = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [language, setLanguage] = useState("en"); // Ngôn ngữ hiện tại
 
-  const changeLanguage =(lgn) =>{
+  const changeLanguage = (lgn) => {
     i18n.changeLanguage(lgn);
-    console.log("change language", lgn)
-  }
+    console.log("change language", lgn);
+  };
+
+  // Xử lý thay đổi ngôn ngữ
+  const handleLanguageChange = (event) => {
+    const selectedLanguage = event.target.value;
+    setLanguage(selectedLanguage);
+    changeLanguage(selectedLanguage);
+  };
 
   return (
     <Box display="flex" justifyContent="end" p={2}>
@@ -42,42 +58,42 @@ const Topbar = () => {
             <DarkModeOutlinedIcon />
           )}
         </IconButton>
-        
-       
 
-       {/* Language switcher - English (USA) */}
-        <Tooltip title="Switch to English">
-          <IconButton
-            sx={{
+        {/* Select chuyển đổi ngôn ngữ */}
+        <Select
+          value={language}
+          onChange={handleLanguageChange}
+          sx={{
+            minWidth: "50px", // Giảm chiều rộng xuống tối thiểu
+            height: "32px", // Chiều cao của thẻ
+            padding: "0 8px", // Giảm padding để tiết kiệm không gian
+            "& .MuiSelect-select": {
               display: "flex",
-              bgcolor: colors.primary[400],
-              borderRadius: "50%", // Bo tròn nút
-              mx: 1,
-              padding: "5px", // Điều chỉnh padding
-            }}
-            onClick={() => changeLanguage("en")}
-          >
-            <img src={usaFlag} alt="USA Flag" width="24" height="24" style={{ borderRadius: "50%" }} />
-          </IconButton>
-        </Tooltip>
-
-        {/* Language switcher - Vietnamese */}
-        <Tooltip title="Chuyển sang Tiếng Việt">
-          <IconButton
-            sx={{
-              display: "flex",
-              bgcolor: colors.primary[400],
-              borderRadius: "50%", // Bo tròn nút
-              mx: 1,
-              padding: "5px", // Điều chỉnh padding
-            }}
-            onClick={() => changeLanguage("vi")}
-          >
-            <img src={vietnamFlag} alt="Vietnam Flag" width="24" height="24" style={{ borderRadius: "50%" }} />
-          </IconButton>
-        </Tooltip>
-      
-        
+              alignItems: "center",
+              justifyContent: "center", // Căn giữa cờ trong thẻ
+              padding: "0", // Loại bỏ padding để thu nhỏ thẻ nhất có thể
+            },
+          }}
+        >
+          <MenuItem value="vi">
+            <img
+              src={vietnamFlag}
+              alt="Tiếng Việt"
+              width="24" // Kích thước cờ nhỏ vừa
+              height="24"
+              style={{ borderRadius: "50%" }}
+            />
+          </MenuItem>
+          <MenuItem value="en">
+            <img
+              src={usaFlag}
+              alt="English"
+              width="24" // Kích thước cờ nhỏ vừa
+              height="24"
+              style={{ borderRadius: "50%" }}
+            />
+          </MenuItem>
+        </Select>
       </Box>
     </Box>
   );
