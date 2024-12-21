ed52bf9b6b2b9004de5a9b90570d60b3a3cf58f5
ChauHuyDien <82036270+idiotman-2212@users.noreply.github.com>
2024-12-21 10:35:31 +0700

    update 21/12

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
 const http = axios.create({
-    //AWS
-    //baseURL: 'http://35.174.1.161:8080/api/v1/',
-
-    //VPS
-    //baseURL: 'http://14.225.253.62:8080/api/v1/',
-
     //local
-    baseURL: 'http://localhost:8080/api/v1/',
+    baseURL: 'http://admin.chauhuydien.id.vn/api/v1/',
+
+    //baseURL: 'http://localhost:8080/api/v1/',
     timeout: 10000, // 10s
     headers: {
         "Content-Type": "application/json",
